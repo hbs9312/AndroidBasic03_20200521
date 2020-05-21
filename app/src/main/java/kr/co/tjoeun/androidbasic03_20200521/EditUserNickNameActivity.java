9d595abbd11a65206c2b4958f1828823a08fdfea
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-21 15:18:58 +0900

    닉네임 수정 화면  - 그림 따로 그려야함

---
+++ app/src/main/java/kr/co/tjoeun/androidbasic03_20200521/EditUserNickNameActivity.java
@@ -0,0 +1,26 @@
+package kr.co.tjoeun.androidbasic03_20200521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditUserNickNameActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_nick_name);
+        setEvents();
+        setValues();
+    }
+
+    @Override
+    public void setEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
