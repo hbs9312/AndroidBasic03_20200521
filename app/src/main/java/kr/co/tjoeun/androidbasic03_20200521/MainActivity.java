5f578fac5eea36ffa43ed35e1c61186c58c6eda9
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-21 15:16:07 +0900

    프로필 화면 초안 작성중

--- app/src/main/java/kr/co/tjoeun/androidbasic03_20200521/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/androidbasic03_20200521/MainActivity.java
@@ -1,6 +1,7 @@
 package kr.co.tjoeun.androidbasic03_20200521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
@@ -13,7 +14,9 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setEvents();
+        setValues();
     }
 
     @Override
